8789e63c25bdc1f76686f4f0f503618aa7f217e7
Zytronium <dastelljes@outlook.com>
2025-12-31 19:07:55 -0600

    Fix build errors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  distDir: 'out',
+  assetPrefix: './',
+  images: {
+    unoptimized: true,
+  },
   reactCompiler: true,
 };
 
